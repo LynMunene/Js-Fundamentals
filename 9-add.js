d1d4aa2e4f33f909fb175e281a9eda189514decf
Lyn Munene <157380414+LynMunene@users.noreply.github.com>
2025-05-23 23:13:21 +0300

    Update 9-add.js

--- 9-add.js
+++ 9-add.js
@@ -1,10 +1,11 @@
 
 
+
 function add(a, b) {
-    return a + b;
-  }
-  
-  const num1 = parseInt(process.argv[2]) || 0;
-  const num2 = parseInt(process.argv[3]) || 0;
-  
-  console.log(add(num1, num2));
+  return a + b;
+}
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+console.log(add(num1, num2));
